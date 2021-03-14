e36f6b3eec0c62e929453eb1e78fb92d9bcb02d3
Alan Tseng <atseng202@gmail.com>
2021-03-14 01:56:51 -0800

    Finish ProfileForm component
    Allows users to edit information like names and emails
    and shows error or success messages on submit

--- src/user/ProfileForm.js
+++ src/user/ProfileForm.js
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import { useHistory } from "react-router-dom";
 import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import JoblyApi from '../api';
 
 /*
  * Renders the ProfileForm
@@ -15,15 +18,24 @@ import Form from "react-bootstrap/Form";
  * App -> Routes -> ProfileForm
  */
 
-function ProfileForm({ currentUser }) {
+function ProfileForm({ currentUser, update }) {
   const [formData, setFormData] = useState({
-    username: currentUser.username,
+    username: currentUser ? currentUser.username : "",
     password: "",
-    firstName: currentUser.firstName,
-    lastName: currentUser.lastName,
-    email: currentUser.email,
+    firstName: currentUser ? currentUser.firstName: "",
+    lastName: currentUser ? currentUser.lastName: "",
+    email: currentUser ? currentUser.email: "",
   });
+  const [errors, setErrors] = useState(null);
+  const [updateSuccess, setUpdateSuccess] = useState(null);
   const history = useHistory();
+
+  console.debug(
+    "ProfileForm",
+    "currentUser: ", currentUser,
+    "formData: ", formData,
+    "errors: ", errors,
+  )
 
   /** Handle input changes and show them on input */
 
@@ -40,25 +52,36 @@ function ProfileForm({ currentUser }) {
   /** Handle submit of edit form */
   async function handleSubmit(evt) {
     evt.preventDefault();
-    function updateUser(data) {
-      return 0;
-    }
-    const result = await updateUser(formData);
-    setFormData({
-      username: currentUser.username,
-      password: "",
-      firstName: currentUser.firstName,
-      lastName: currentUser.lastName,
-      email: currentUser.email,
-    });
 
+    // username cannot be edited as per our update schema
+    let editableData = {
+      firstName: formData.firstName,
+      lastName: formData.lastName,
+      email: formData.email,
+      password: formData.password,
+    }
+
+    let username = formData.username;
+    let changedUser;
+
+    const result = await update(username, editableData);
+    
     if (result.success === true) {
+      setUpdateSuccess("Successfully updated profile!")
+      setErrors(null);
+      setFormData( (prevData) => (
+        {
+          ...prevData,
+          password: "",
+        }
+      ));
     } else {
-      // TODO: set up error handling in presentable format
-      alert("Failed to edit profile");
+      setErrors(result.errors.join(""));
+      setUpdateSuccess(null);
     }
+    
   }
-
+  
   /* Helper function for form validation */
   function notDone() {
     const invalidInputArr = Object.keys(formData).filter((key) => {
@@ -68,7 +91,88 @@ function ProfileForm({ currentUser }) {
     return invalidInputArr.length > 0;
   }
 
-  return <div>ProfileForm goes here!</div>;
+  let errorMsg = errors ? (
+    <div className="alert alert-danger">{errors}</div>
+  ) : null;
+
+  let successMsg = updateSuccess ? (
+    <div className="alert alert-success">{updateSuccess}</div>
+  ) : null;
+
+  if (!currentUser) return <div>Loading...</div>;
+  return (
+    <Container>
+      <h2 className="my-4">Edit Profile!</h2>
+
+      {errorMsg}
+
+      {successMsg}
+
+      <Form onSubmit={handleSubmit}>
+        <Form.Group controlId="username">
+          <Form.Label>Username</Form.Label>
+          <Form.Control
+            type="text"
+            name="username"
+            value={formData.username}
+            onChange={handleChange}
+            required
+            disabled={true}
+          />
+        </Form.Group>
+
+
+        <Form.Group controlId="firstName">
+          <Form.Label>First name</Form.Label>
+          <Form.Control
+            type="text"
+            name="firstName"
+            value={formData.firstName}
+            onChange={handleChange}
+            required
+          />
+        </Form.Group>
+
+        <Form.Group controlId="lastName">
+          <Form.Label>Last name</Form.Label>
+          <Form.Control
+            type="text"
+            name="lastName"
+            value={formData.lastName}
+            onChange={handleChange}
+            required
+          />
+        </Form.Group>
+
+        <Form.Group controlId="email">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </Form.Group>
+
+        <Form.Group controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+        </Form.Group>
+
+        <Button variant="primary" type="submit" disabled={notDone()}>
+          Submit
+        </Button>
+      </Form>
+      
+    </Container>
+  )  
 }
 
 export default ProfileForm;
